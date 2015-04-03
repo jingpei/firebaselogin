a357b75e282028973bb1c3e16e79044fdf9e03b9
jingpei <jingjpei@gmail.com>
2015-04-02 21:20:28 -0400

    firebase and login

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var graphApp = angular.module("graphApp", ["ngRoute", "appControllers"]);
+var graphApp = angular.module("graphApp", ["ngRoute", "appControllers", "firebase"]);
 
 var appControllers = angular.module("appControllers", []);
 
@@ -6,7 +6,7 @@ var appControllers = angular.module("appControllers", []);
 graphApp.config(["$routeProvider", function( $routeProvider ){
 	$routeProvider
 		.when('/graph', {controller: 'graphControllers', templateUrl: 'partials/graph.html'})
-		.when('/login', {templateUrl: 'partials/login.html'})
-		.when('/register', {templateUrl: 'partials/register.html'})
+		.when('/login', {controller: 'registrationCtrl', templateUrl: 'partials/login.html'})
+		.when('/register', {controller: 'registrationCtrl', templateUrl: 'partials/register.html'})
 		.otherwise({ redirectTo: '/login' });
 }]);
--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,69 +1,86 @@
-graphApp.controller("graphControllers", function( $scope ){
+graphApp.controller("graphControllers", function( $scope, $firebaseArray ){
 
+  var ref = new Firebase('https://jp-dataviz.firebaseio.com/data');
+  $scope.dbData = $firebaseArray(ref);
+  //var trueArr = $firebaseArray(ref).$asArray();
+  console.log($scope.dbData);
   $scope.width = 600;
   $scope.height = 250;
 
   $scope.yAxis = 'Booty Haul';  
   $scope.xAxis = '2015';
 
-  // $scope.max = Math.max(($scope.data).map(function(obj){
-  //   return obj.value;
-  // }));
-
-  // console.log($scope.max)
-
-  $scope.maxtwo = 342;
     // Data 
-    $scope.data = [
-    {
-      label: 'January',
-      value: 36
-    },
-    {
-      label: 'February',
-      value: 54
-    },
-    {
-      label: 'March',
-      value: 62
-    },
-    {
-      label: 'April',
-      value: 82
-    },
-    {
-      label: 'May',
-      value: 96
-    },
-    {
-      label: 'June',
-      value: 104
-    },
-    {
-      label: 'July',
-      value: 122
-    },
-    {
-      label: 'August',
-      value: 152
-    },
-    {
-      label: 'September',
-      value: 176
-    },
-    {
-      label: 'October',
-      value: 180
-    },
-    {
-      label: 'November',
-      value: 252
-    },
-    {
-      label: 'December',
-      value: 342
-    }
-    ];
+    // $scope.data = [
+    // {
+    //   label: 'January',
+    //   value: 36
+    // },
+    // {
+    //   label: 'February',
+    //   value: 54
+    // },
+    // {
+    //   label: 'March',
+    //   value: 62
+    // },
+    // {
+    //   label: 'April',
+    //   value: 82
+    // },
+    // {
+    //   label: 'May',
+    //   value: 96
+    // },
+    // {
+    //   label: 'June',
+    //   value: 104
+    // },
+    // {
+    //   label: 'July',
+    //   value: 122
+    // },
+    // {
+    //   label: 'August',
+    //   value: 152
+    // },
+    // {
+    //   label: 'September',
+    //   value: 176
+    // },
+    // {
+    //   label: 'October',
+    //   value: 180
+    // },
+    // {
+    //   label: 'November',
+    //   value: 252
+    // },
+    // {
+    //   label: 'December',
+    //   value: 342
+    // },
+    // {
+    //   label: 'Hello',
+    //   value: 50
+    // }
+    // ];
+
+    $scope.max = 342;
+
+  // $scope.max = Math.max.apply(Math, (($scope.dbData).map(function(obj){
+  //   return obj.value;
+  // })));
+
+});
+
+graphApp.controller("registrationCtrl", function( $scope, $firebaseArray ){
+  $scope.login = function( login_info ){
+    alert("submitted");
+  }
+  $scope.$on("$viewContentLoaded", function(){
+    console.log($scope.myform);
+  })
 
 });
 
